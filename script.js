7586adf4df3b399470e2e3c1a99dd556035d4f16
VikaKhaustova <haustovavika@gmail.com>
2024-09-24 17:21:09 +0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+
+
+// // Отримуємо кнопку "Кошик"
+// let angl = document.querySelector('angl')
+
+// // Навішуємо обробник подій на клік кнопки "Кошик"
+// angl.addEventListener("click", function () {
+//     // Переходимо на сторінку кошика
+//     window.location.assign('index-angl.html')
+// })
