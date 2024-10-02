a636ebbf662cc5f317d0a700c36c4297a679ba1f
lisa228-star <lisa.chernova.002@gmail.com>
2024-10-02 16:37:59 +0200

    darktheme

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 
 
-
-// // Отримуємо кнопку "Кошик"
-// let angl = document.querySelector('angl')
-
-// // Навішуємо обробник подій на клік кнопки "Кошик"
-// angl.addEventListener("click", function () {
-//     // Переходимо на сторінку кошика
-//     window.location.assign('index-angl.html')
-// })
+document.getElementById('toggle').addEventListener('change', function() {
+    document.body.classList.toggle('dark-theme', this.checked);
+  });
